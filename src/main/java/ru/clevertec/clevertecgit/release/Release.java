b4bfc4da1189da3f1a57038d6771d9b3906004a6
bumblebear <aastatkevich@gmail.com>
2023-02-05 21:31:59 +0300

    rel:added equals and hashcode

--- src/main/java/ru/clevertec/clevertecgit/release/Release.java
+++ src/main/java/ru/clevertec/clevertecgit/release/Release.java
@@ -1,5 +1,7 @@
 package ru.clevertec.clevertecgit.release;
 
+import java.util.Objects;
+
 public class Release {
     private String field;
 
@@ -13,5 +15,18 @@ public class Release {
 
     public void setField(String field) {
         this.field = field;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Release release = (Release) o;
+        return Objects.equals(field, release.field);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(field);
     }
 }
