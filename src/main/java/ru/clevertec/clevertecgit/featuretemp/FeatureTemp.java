a48131f51fffcd1fbd9db67cfdbb41a1d3f92279
bumblebear <aastatkevich@gmail.com>
2023-02-05 21:30:39 +0300

    feat:added constructor

--- src/main/java/ru/clevertec/clevertecgit/featuretemp/FeatureTemp.java
+++ src/main/java/ru/clevertec/clevertecgit/featuretemp/FeatureTemp.java
@@ -3,4 +3,9 @@ package ru.clevertec.clevertecgit.featuretemp;
 public class FeatureTemp {
     private String field;
     private String anotherField;
+
+    public FeatureTemp(String field, String anotherField) {
+        this.field = field;
+        this.anotherField = anotherField;
+    }
 }
