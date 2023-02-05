0ad00f602c344efe5b34357d4256ac77e60a527b
bumblebear <aastatkevich@gmail.com>
2023-02-05 21:29:44 +0300

    feat: added equals and hashcode

--- src/main/java/ru/clevertec/clevertecgit/feature/Feature.java
+++ src/main/java/ru/clevertec/clevertecgit/feature/Feature.java
@@ -1,5 +1,20 @@
 package ru.clevertec.clevertecgit.feature;
 
+import java.util.Objects;
+
 public class Feature {
     private String field;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feature feature = (Feature) o;
+        return Objects.equals(field, feature.field);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(field);
+    }
 }
